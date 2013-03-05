56970899bd49a13831d03c5b38d0d6dbc460ddc2
Logic-Seeker <shobhabstha@gmail.com>
2013-03-05 20:00:12 +0545

    initial

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,5 @@
+class MainController < ApplicationController
+layout 'test'
+  def index
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SchoolApp::Application.config.secret_token = 'b306e47db668ea3522e5484be92e28e2fef66e94aa369fc2c914eb5a37ceb904e26b6c664e943a1b82a098efea749331e91cb9fc92bc9cbb2b394f460d652571'
